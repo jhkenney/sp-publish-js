8ade2f8abe187b4a064bad960ad2bbdfc4e74ab5
Jake Kenney <jkenney320@gmail.com>
2022-10-25 14:23:05 -0700

    include files without extensions

--- dist/index.js
+++ dist/index.js
@@ -76622,7 +76622,7 @@ try {
       password: spPass,
   };
   var fileOptions = {
-    glob: `${process.env.GITHUB_WORKSPACE}\\${outputFolder}\\**\\*.*`,
+    glob: `${process.env.GITHUB_WORKSPACE}\\${outputFolder}\\**\\*`,
     folder: spFolder, 
   };
   var path = `${process.env.GITHUB_WORKSPACE}\\${outputFolder}\\**\\`
